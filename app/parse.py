0c8a044d46dbc9fe05051b970e486f791a615966
test <test@gmail.com>
2022-12-08 21:56:41 +0200

    first solution

--- app/parse.py
+++ app/parse.py
@@ -1,9 +1,11 @@
-from dataclasses import dataclass
-from urllib.parse import urljoin
+import csv
 
+from dataclasses import dataclass, fields, astuple
+from time import sleep
 
-BASE_URL = "https://webscraper.io/"
-HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/more/")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @dataclass
@@ -15,8 +17,91 @@ class Product:
     num_of_reviews: int
 
 
+_driver: WebDriver | None = None
+HEADERS = [field.name for field in fields(Product)]
+URL_BASE = "https://webscraper.io/"
+
+URLS_FOR_PARSING = {
+    "home": "https://webscraper.io/test-sites/e-commerce/more",
+    "laptops": "https://webscraper.io/test-sites/e-commerce/more/computers/laptops",
+    "computers": "https://webscraper.io/test-sites/e-commerce/more/computers",
+    "tablets": "https://webscraper.io/test-sites/e-commerce/more/computers/tablets",
+    "phones": "https://webscraper.io/test-sites/e-commerce/more/phones",
+    "touch": "https://webscraper.io/test-sites/e-commerce/more/phones/touch",
+}
+
+
+def get_driver() -> WebDriver:
+    return _driver
+
+
+def set_driver(new_driver: WebDriver) -> None:
+    global _driver
+    _driver = new_driver
+
+
+def write_to_csv(path_to_csv: str, products: [Product]) -> None:
+    with open(path_to_csv, "w", encoding="UTF-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(HEADERS)
+        writer.writerows([astuple(product) for product in products])
+
+
+def accept_cookies() -> None:
+    cookie = get_driver().find_elements(By.ID, "cookieBanner")
+    if len(cookie) == 0:
+        return
+
+    is_hidden = "display: none" in cookie[0].get_attribute("style")
+    if not is_hidden:
+        cookie_btn = get_driver().find_element(By.CLASS_NAME, "acceptCookies")
+        cookie_btn.click()
+
+
+def over_more_pagination() -> None:
+    more_btn = get_driver().find_elements(By.CLASS_NAME, "ecomerce-items-scroll-more")
+    if len(more_btn) == 0:
+        return
+    more_btn = more_btn[0]
+
+    while True:
+        if more_btn.is_displayed():
+            more_btn.click()
+            sleep(0.1)
+        else:
+            break
+
+
+def get_product_info(card) -> Product:
+    title = card.find_element(By.CSS_SELECTOR, ".caption .title").get_attribute("title")
+    price = float(card.find_element(By.CSS_SELECTOR, ".caption .price").text.replace("$", ""))
+    description = card.find_element(By.CSS_SELECTOR, ".description").text
+    num_of_reviews = int(card.find_element(By.CSS_SELECTOR, ".ratings p").text.split()[0])
+    rating = len(card.find_elements(By.CSS_SELECTOR, ".ratings .glyphicon-star"))
+
+    return Product(
+        title=title,
+        price=price,
+        description=description,
+        num_of_reviews=num_of_reviews,
+        rating=rating,
+    )
+
+
 def get_all_products() -> None:
-    pass
+    with webdriver.Chrome() as new_driver:
+        set_driver(new_driver)
+
+        for category_name, url in URLS_FOR_PARSING.items():
+            get_driver().get(url)
+            accept_cookies()
+            over_more_pagination()
+
+            products = []
+            for card in get_driver().find_elements(By.CLASS_NAME, "thumbnail"):
+                products.append(get_product_info(card))
+
+            write_to_csv(f"{category_name}.csv", products)
 
 
 if __name__ == "__main__":
